c30c8f1a1084567388b0b2ee1a222d01dd30ff2f
EdwinAlexanderBernardinoMoran <edwinalexanderbernardinomoran@gmail.com>
2026-04-12 20:29:01 -0600

    s2 - Manejo de errores

--- cripto-random/src/App.tsx
+++ cripto-random/src/App.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 
 const getCryptoNumber = async (): Promise<number> => {
+  throw 'Failed to fetch number';
+
   const resp = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new').then(response => response.json())
   return Number(resp);
 }
@@ -10,6 +12,7 @@ export const App = () => {
   const { data: number, isLoading, isFetching, error, refetch } = useQuery({
     queryKey: ['crypto-number'],
     queryFn: getCryptoNumber,
+    retry: false,
   })
 
   return (
